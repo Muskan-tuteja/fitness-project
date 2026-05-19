e7374cddd09197118d894bc9d3b10ed2bd8e9ea2
muskan <muskantuteja714@gmail.com>
2026-05-19 11:35:51 +0530

    project start

--- fitness-microservices/userservice/src/main/java/com/fitness/userservice/models/User.java
+++ fitness-microservices/userservice/src/main/java/com/fitness/userservice/models/User.java
@@ -2,9 +2,19 @@ package com.fitness.userservice.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "users")
+@Data
 public class User {
+    private int id;
+    private String email;
+   private String password;
+   private String firstname;
+   private String lastname;
+   private UserRole role = UserRole.USER;
 
 
 }
